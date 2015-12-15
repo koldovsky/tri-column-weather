009211c516848ecfb8a0262052333cb380666254
koldovsky <koldovsky@yahoo.com>
2015-12-15 16:02:30 +0000

    Num to str

--- scripts/index.js
+++ scripts/index.js
@@ -3,9 +3,15 @@ $(function(){
         'http://api.openweathermap.org/data/2.5/forecast/daily?q=' 
         + 'Lviv' + '&APPID=85dd9ac97b8b9d0cbc5f814a5b96ffa6&cnt=3&units=metric' + '&lang=' + 'ua' + '&callback=?',
         function (data) {
-            $('#todayTemp').html(data.list[0].temp.day);
-            $('#tomorrowTemp').html(data.list[1].temp.day);
-            $('#afterTomorrowTemp').html(data.list[2].temp.day);
+            $('#todayTemp').html(tempNumToStr(data.list[0].temp.day));
+            $('#tomorrowTemp').html(tempNumToStr(data.list[1].temp.day));
+            $('#afterTomorrowTemp').html(tempNumToStr(data.list[2].temp.day));
+            $('#todayPressure').html(data.list[0].pressure);
+            $('#tomorrowPressure').html(data.list[1].pressure);
+            $('#afterTomorrowPressure').html(data.list[2].pressure);
         }
     );
+    function tempNumToStr(temp) {
+       return (temp > 0)? 'Тепло' : 'Холодно';
+    }
 });
